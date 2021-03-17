9ef1f01e19ce6142bfc139679ed355b56cbed632
Laura Currier <78007303+joliemie@users.noreply.github.com>
2021-03-17 12:15:44 -0600

    Add files via upload

---
+++ vss.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string sestiOne;
+    string sestiTwo;
+    string sestiThree;
+    string sestiFour;
+    string sestiFive;
+    string sestiSix;
+
+    cout << "enter the last words for your sestina, in order per the first stanza. ";
+    getline (cin, sestiOne);
+    getline (cin, sestiTwo);
+    getline (cin, sestiThree);
+    getline (cin, sestiFour);
+    getline (cin, sestiFive);
+    getline (cin, sestiSix);
+
+    cout << sestiOne << endl;
+    cout << sestiTwo << endl;
+    cout << sestiThree << endl;
+    cout << sestiFour << endl;
+    cout << sestiFive << endl;
+    cout << sestiSix << endl;
+
+    cout << sestiSix << endl;
+    cout << sestiOne << endl;
+    cout << sestiFive << endl;
+    cout << sestiTwo << endl;
+    cout << sestiFour << endl;
+    cout << sestiThree << endl;
+
+    cout << sestiThree << endl;
+    cout << sestiSix << endl;
+    cout << sestiFour << endl;
+    cout << sestiOne << endl;
+    cout << sestiTwo << endl;
+    cout << sestiFive << endl;
+
+    cout << sestiFive << endl;
+    cout << sestiThree << endl;
+    cout << sestiTwo << endl;
+    cout << sestiSix << endl;
+    cout << sestiOne << endl;
+    cout << sestiFour << endl;
+
+    cout << sestiFour << endl;
+    cout << sestiOne << endl;
+    cout << sestiFive << endl;
+    cout << sestiThree << endl;
+    cout << sestiSix << endl;
+    cout << sestiTwo << endl;
+
+    cout << sestiTwo << endl;
+    cout << sestiFour << endl;
+    cout << sestiSix << endl;
+    cout << sestiFive << endl;
+    cout << sestiThree << endl;
+    cout << sestiOne << endl;
+
+    cout << sestiTwo << " " << sestiFive << endl;
+    cout << sestiFour << " " << sestiThree << endl;
+    cout << sestiSix << " " << sestiOne << endl;
+
+ return 0;
+}
